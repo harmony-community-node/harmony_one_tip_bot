ac44fc54390e2047c302b83408edf5162015c062
harmony-community-node <68701699+harmony-community-node@users.noreply.github.com>
2020-11-29 23:18:58 -0500

    Create secretes.js

---
+++ secretes.js
@@ -0,0 +1,6 @@
+module.exports.TWITTER_CONSUMER_KEY= "<Consumer Key>";
+module.exports.TWITTER_CONSUMER_SECRET = "<Consumer Secret>";
+module.exports.TWITTER_ACCESS_TOKEN = "<Access Token>";
+module.exports.TWITTER_ACCESS_TOKEN_SECRET = "<Access Token Scret>";
+module.exports.TWITTER_WEBHOOK_ENV = "<ENV NAME>";
+module.exports.NGROK_AUTH_TOKEN = "";
